f6002941fed921640cf1e01598fec925293e3665
Abtuly <abdullah.khawahir@gmail.com>
2023-01-31 21:14:34 +0300

    the loader cachs the last request incase of the API reject the get request. the loader will respond with the last request if the rejection happens

--- server_loader.py
+++ server_loader.py
@@ -1,16 +1,41 @@
 import requests
+import os
+import json
 
 
 class server_loader:
-    server = None
 
     def __init__(self, server_target_id: str):
-        servers = requests.get(
-            "https://servers.realitymod.com/api/ServerInfo").json()["servers"]
-        servers = list(servers)
-        for server in servers:
-            if server["serverId"] == server_target_id:
-                self.server = server
+        server_loader._create_file(server_target_id)
+        servers_info_request = requests.get(
+            "https://servers.realitymod.com/api/ServerInfo")
+        if servers_info_request.ok:
+            servers = list(servers_info_request.json()["servers"])
+            for server in servers:
+                if server["serverId"] == server_target_id:
+                    self.server = server
+                    server_loader._writeResonseToFile(
+                        server_target_id, self.server)
+        else:
+            self.server = json.loads(
+                server_loader._get_last_request(server_target_id))
+
+    def _get_last_request(serverID: str):
+        fileName = f"{serverID}.json"
+        file = open(fileName, "r")
+        file_content = file.read()
+        file.close()
+        return file_content
+
+    def _create_file(serverID: str):
+        fileName = f"{serverID}.json"
+        if not os.path.exists(f"{serverID}.json"):
+            open(fileName, "w").close()
+
+    def _writeResonseToFile(serverID, response: dict):
+        file = open(f"{serverID}.json", "w")
+        file.write(json.dumps(response))
+        file.close()
 
     def get_map_name(self):
         return self.getProperties()["mapname"]
@@ -28,6 +53,7 @@ class server_loader:
         return self.getProperties()["numplayers"]
 
     def getProperties(self):
+        # print(self.server)
         return self.server["properties"]
 
     def get_team_one(self):
